a081ee188cabaa620cfc8d200d03324b21e09e56
marufbytes <marufahammed215@gmail.com>
2025-12-31 02:43:40 +0600

    aa

---
+++ Module_1/input.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int x;
+    char c;
+    cin>>x>>c;
+    cout<<x<<c<< endl;
+
+    char a = 'A';
+    int asci = a;
+    cout<<asci<<endl;
+    cout << (int)a<<endl;
+
+    return 0;
+}
